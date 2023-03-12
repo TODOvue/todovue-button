c28a4f765977eccfb465e4b0f2b2c830ceadfe7a
Cristhian Daza <cristian.d.b@hotmail.com>
2023-03-11 23:18:06 -0500

    feat(*) Component demo added

---
+++ src/Demo.vue
@@ -0,0 +1,84 @@
+<template>
+  <tv-demo>
+    <h2 class="title">TODOvue Button</h2>
+    <div v-for="(demo, index) in demos" :key="`demo-${index}`" class="demo">
+      <h2>{{ demo.title }}</h2>
+      <tv-button
+        :is-small="demo.buttonProps.isSmall"
+        :is-large="demo.buttonProps.isLarge"
+        :is-rounded="demo.buttonProps.isRounded"
+        :is-outlined="demo.buttonProps.isOutlined"
+        :is-success="demo.buttonProps.isSuccess"
+        :icon="demo.buttonProps.icon"
+        :is-full="demo.buttonProps.isFull"
+      >
+        {{ demo.buttonText }}
+      </tv-button>
+    </div>
+  </tv-demo>
+</template>
+
+<script>
+import TvButton from "@/component/TvButton.vue";
+import TvDemo from "todovue-demo";
+export default {
+  name: "DemoButton",
+  data() {
+    return {
+      demos: [
+        {
+          title: "TvButton Small",
+          buttonProps: { isSmall: true },
+          buttonText: "Press me",
+        },
+        {
+          title: "TvButton Default",
+          buttonProps: {},
+          buttonText: "Press me",
+        },
+        {
+          title: "TvButton Large",
+          buttonProps: { isLarge: true },
+          buttonText: "Press me",
+        },
+        {
+          title: "TvButton Rounded",
+          buttonProps: { isRounded: true },
+          buttonText: "Press me",
+        },
+        {
+          title: "TvButton Outlined",
+          buttonProps: { isOutlined: true },
+          buttonText: "Press me",
+        },
+        {
+          title: "TvButton Success",
+          buttonProps: { isSuccess: true },
+          buttonText: "Press me",
+        },
+        {
+          title: "TvButton Icon (Add)",
+          buttonProps: { icon: "add", isSuccess: true },
+          buttonText: "Press me",
+        },
+        {
+          title: "TvButton Icon (remove)",
+          buttonProps: { icon: "remove" },
+          buttonText: "Press me",
+        },
+        {
+          title: "TvButton Full",
+          buttonProps: { isFull: true },
+          buttonText: "Press me",
+        },
+      ],
+    };
+  },
+  components: {
+    TvDemo,
+    TvButton,
+  },
+};
+</script>
+
+<style lang="scss"></style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from "vue";
-import TvButton from "./component/TvButton.vue";
+import Demo from "./Demo.vue";
 import "@/assets/css/variables.css";
 import "@/assets/css/global.css";
+import "todovue-demo/dist/tvdemo.css";
+import "todovue-button/dist/tvbutton.css";
 
-createApp(TvButton).mount("#todovue-button");
+createApp(Demo).mount("#todovue-button");
