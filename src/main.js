420c193094aba296b74654b7abbb34f7cf91721e
Cristhian Daza <cristian.d.b@hotmail.com>
2023-03-11 21:27:59 -0500

    fix(*) Added import

--- src/main.js
+++ src/main.js
@@ -2,5 +2,6 @@ import { createApp } from "vue";
 import TvButton from "./component/TvButton.vue";
 import "@/assets/css/variables.css";
 import "@/assets/css/global.css";
+import "todovue-button/dist/tvbutton.css";
 
 createApp(TvButton).mount("#todovue-button");
