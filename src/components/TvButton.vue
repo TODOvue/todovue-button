77c24733a98897d8bc514f048e16c0dca2ad7f0d
Cristhian Daza <cristian.d.b@hotmail.com>
2025-02-22 18:33:53 -0500

    feat(*): add initial implementation of TvButton component

--- src/components/TvButton.vue
+++ src/components/TvButton.vue
@@ -1,9 +1,11 @@
 <script setup>
+
 </script>
 
 <template>
-  <h1>TODOvue Button</h1>
+
 </template>
 
 <style scoped>
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
 import './style.css'
-import App from './App.vue'
+import TvButton from './demo/Demo.vue'
 
-createApp(App).mount('#app')
+createApp(TvButton).mount('#tv-button')
