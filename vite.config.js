5f30340b170735cb76014c92c5cd3c8423ab0891
Cristhian Daza <cristian.d.b@hotmail.com>
2025-02-22 22:42:51 -0500

    chore(build): add vite-plugin-css-injected-by-js and configure TvButton library build

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,30 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), cssInjectedByJsPlugin()],
+  build: {
+    lib: {
+      entry: "src/components/TvButton.vue",
+      name: "TvButton",
+      fileName: format => `todovue-button.${format}.js`,
+      formats: ["es", "cjs"]
+    },
+    rollupOptions: {
+      external: ["vue"],
+      output: {
+        globals: {
+          vue: "Vue"
+        }
+      }
+    }
+  },
   css: {
     preprocessorOptions: {
       scss: {
-        api: 'modern-compiler'
-      },
-    },
-  },
+        api: "modern-compiler"
+      }
+    }
+  }
 });
